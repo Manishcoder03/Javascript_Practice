37e846d41728b4f23483758b907bac5126403367
Manishcoder03 <138953906+Manishcoder03@users.noreply.github.com>
2023-07-14 05:22:27 +0000

    comparison

--- 1.basic/3-conversion.js
+++ 1.basic/3-conversion.js
@@ -3,5 +3,13 @@ let score = "33hf"
 console.log(typeof score);
 console.log(typeof (score));
 let valueInNumber = Number(score);
-console.log(typeof (valueInNumber));
-console.log(valueInNumber);
+//console.log(typeof (valueInNumber));
+//console.log(valueInNumber);
+// operation
+let value = 3
+let negValue = -value
+console.log(negValue);
+str1 = 2
+str2 = 4
+str3 = str1 + str2
+console.log(str3);
---
+++ 1.basic/4-comparison.js
@@ -0,0 +1,4 @@
+console.log(4 > 3);
+console.log(4 < 3);
+console.log("4" >= 5);
+console.log("4" <= 5);
