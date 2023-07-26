3b0e075900d609b2a3c444ece6cf8b68fb976f51
Manishcoder03 <138953906+Manishcoder03@users.noreply.github.com>
2023-07-26 07:41:41 +0000

    6.nums and maths

---
+++ 1.basic/6-nums_and_maths.js
@@ -0,0 +1,13 @@
+const score = 40
+console.log(score);
+const balance = new Number(400)
+console.log(balance);
+console.log(balance.toString().length);
+
+const otherName = 4312.4512
+console.log(otherName.toPrecision(3));
+
+const hundreads = 40000000
+console.log(hundreads.toLocaleString('en-IN'));
+
+console.log(Math);
