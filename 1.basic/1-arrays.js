810138205dfebc70aea051107624456a926f0a5d
Manishcoder03 <138953906+Manishcoder03@users.noreply.github.com>
2023-08-02 14:12:28 +0000

    1-arrays.js

---
+++ 1.basic/1-arrays.js
@@ -0,0 +1,19 @@
+const myArr = [0,1,2,3,4,5]
+const myHeroes = ["manish", "shivam", "raju"]
+
+const myArr2 = new Array(1,2,3)
+console.log(myArr[1]);
+myArr.push(6) // push method using a js
+myArr.push(7)
+console.log(myArr);
+myArr.pop
+console.log(myArr); //pop method are used in last Element are delete
+myArr.unshift(8) //unshift are used in one element are adding starting
+console.log(myArr);
+myArr.shift() // shift method in which first elements are deleted
+console.log(myArr);
+console.log(myArr.includes(7)); // includes method in which thoes number are present then output becomes true either false
+console.log(myArr.indexOf(5)); // indexOf c
+
+
+
