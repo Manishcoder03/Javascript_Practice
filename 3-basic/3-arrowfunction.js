0b06940c51fd485af44ba8f11f9e97850fa0a7ce
Manishcoder03 <138953906+Manishcoder03@users.noreply.github.com>
2023-08-06 15:15:07 +0000

    3-arrowfunction.js

--- 3-basic/3-arrowfunction.js
+++ 3-basic/3-arrowfunction.js
@@ -1,12 +1,30 @@
-const user = {
-    username: "Manish",
-    price: 999,
+// const user = {
+//     username: "Manish",
+//     price: 999,
 
-    welcomeMessage: function (){
-        console.log(`${this.username}, welcome to website`);
-    }
+//     welcomeMessage: function (){
+//         console.log(`${this.username}, welcome to website`);
+//     }
+// }
+
+// user.welcomeMessage()
+// user.username = "bhai"
+// user.welcomeMessage()
+
+// const chai = () => {
+//     let username = "manish",
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const addTwo = (num1, num2) => num1 + num2
+
+
+// console.log(addTwo(2, 4));
+
+const addTwo = (num1, num2) => {
+    return num1 + num2
 }
 
-user.welcomeMessage()
-user.username = "bhai"
-user.welcomeMessage()
+console.log(addTwo(4, 5));
