7c7889ee7aec0a8050b2b90d6e2e122ce3971bb3
Manishcoder03 <138953906+Manishcoder03@users.noreply.github.com>
2023-08-07 15:26:09 +0000

    switch

---
+++ 4-controlflow/switch.js
@@ -0,0 +1,19 @@
+const month = 3
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("not matching");
+        break;
+}
