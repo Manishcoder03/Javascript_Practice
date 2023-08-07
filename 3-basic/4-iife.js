6c5106bbf25de5d570285ab461804e7de3794d99
Manishcoder03 <138953906+Manishcoder03@users.noreply.github.com>
2023-08-07 14:10:02 +0000

    one.js

---
+++ 3-basic/4-iife.js
@@ -0,0 +1,5 @@
+// immediately invoked function expression
+
+(function chai(){
+    console.log(`DB connected`);
+})()
---
+++ 4-controlflow/one.js
@@ -0,0 +1,52 @@
+// < less than
+// > grater than
+// == equal
+// <= less than equal
+// >= grater than equal
+// != not equal
+// === cheaking the string
+
+// const isLoggedIn = true
+// temperture = 100
+// if (temperture < 50) {
+//     console.log("temperture is less than 50");
+// } else {
+//     console.log("temperture is grater than 50");
+// }
+// console.log("up to date Temperture");
+
+// const isLoggedIn = true
+// const userName = "manish"
+// weight = 39
+// if (weight < 40) {
+//     console.log("your Weight is less Than 40");
+//     console.log("you are Not Sutaible");
+// } else {
+//     console.log("your weight is Higher Than 40");
+//     console.log("your Weight is perfect");
+// }
+// console.log("thnx for comming");
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`user power: ${power}`);
+
+    
+// }
+
+// const userName = "manish"
+// const surname = "yadav"
+
+// if (username && surnmae) {
+//     console.log("this is full name");
+// }
+
+const isLoggedIn = true
+const debitCard = true
+
+if (isLoggedIn && debitCard) {
+    console.log("allow to a buy a courses");
+    
+}
