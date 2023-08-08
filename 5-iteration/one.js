56574ec82d61d27070fafb2bbe1582ac064d7575
Manishcoder03 <138953906+Manishcoder03@users.noreply.github.com>
2023-08-08 07:23:51 +0000

    for loop

---
+++ 5-iteration/one.js
@@ -0,0 +1,23 @@
+// for (let i = 0; i <= 10; index++) {
+//     const element = index;
+//     console.log(element);
+    
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`this is inner: ${i}`);
+//     for (let j = 0; j <= 10; j++) {
+//     console.log(`this is outer ${j} and this is inner ${i}`);
+        
+//     }
+    
+// }
+
+for (let index = 1; index <= 20; index++) {
+    if (index==5) {
+        console.log("detected of 5 numbers");
+        break
+    }
+    console.log(`this number is coutable ${index}`);
+    
+}
